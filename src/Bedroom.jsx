ae842470c114a611216b7cfee1a1030ec1298dad
Drew Neece <dneece5@aggienetwork.com>
2023-01-23 20:40:16 -0600

    render Bedrooms

---
+++ src/Bedroom.jsx
@@ -0,0 +1,9 @@
+const Bedroom = (props) => {
+  return (
+    < >
+      <h2>Bedroom {props.num}</h2>
+    </>
+  )
+}
+
+export default Bedroom
--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -1,11 +1,15 @@
 import LivingRoom from "./LivingRoom"
 import Kitchen from "./Kitchen"
+import Bedroom from "./Bedroom"
 
 const FloorPlan = () => {
+  const bedNum = [1, 2, 3]
   return (
     < >
       <div><LivingRoom /></div>
+      <div><Bedroom num={bedNum[1]}/></div>
       <div><Kitchen /></div>
+      <div><Bedroom num={bedNum[0]}/></div>
     </>
   )
 }
