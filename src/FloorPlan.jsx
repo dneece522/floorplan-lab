2a9f050b3d7fc5b1dccc4a0a0e383f996f744198
Drew Neece <dneece5@aggienetwork.com>
2023-01-23 20:45:19 -0600

    render Bathrooms

--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -1,6 +1,7 @@
 import LivingRoom from "./LivingRoom"
 import Kitchen from "./Kitchen"
 import Bedroom from "./Bedroom"
+import Bath from "./Bath"
 
 const FloorPlan = () => {
   const bedNum = [1, 2, 3]
@@ -10,6 +11,9 @@ const FloorPlan = () => {
       <div><Bedroom num={bedNum[1]}/></div>
       <div><Kitchen /></div>
       <div><Bedroom num={bedNum[0]}/></div>
+      <div><Bath size="Full"/></div>
+      <div><Bedroom num={bedNum[2]}/></div>
+      <div><Bath size="Half"/></div>
     </>
   )
 }
